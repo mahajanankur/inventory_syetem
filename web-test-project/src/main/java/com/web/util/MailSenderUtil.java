605d2c60604b054d7b6c98d18825f264400961f0
mahajanankur <mahajanankur.nith@gmail.com>
2015-08-09 23:56:04 +0530

    added new functionality "send mail". STILL WORKING!!

---
+++ web-test-project/src/main/java/com/web/util/MailSenderUtil.java
@@ -0,0 +1,51 @@
+package com.web.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+/**
+ * @author ankur
+ * 
+ */
+public class MailSenderUtil {
+
+	@Autowired
+	private MailSender	mailSender;
+
+	/**
+	 * This method is used to set all the attributes for SimpleMailMessage to
+	 * invoke the MailSender's send method.
+	 * 
+	 * @param from
+	 * @param to
+	 * @param subject
+	 * @param message
+	 */
+	private void sendMail(String from, String to, String subject, String message) {
+
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setFrom(from);
+		mailMessage.setTo(to);
+		mailMessage.setSubject(subject);
+		mailMessage.setText(message);
+
+		mailSender.send(mailMessage);
+	}
+
+	/**
+	 * @return the mailSender
+	 */
+	public MailSender getMailSender() {
+		return mailSender;
+	}
+
+	/**
+	 * @param mailSender
+	 *            the mailSender to set
+	 */
+	public void setMailSender(MailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+}
